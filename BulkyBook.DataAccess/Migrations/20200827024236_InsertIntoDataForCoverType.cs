60ddf96ab4ac45d37049365eea41698f737ccca8
c30541 <c30541@gmail.com>
2020-08-27 14:04:29 +0800

    section8 - complete

---
+++ BulkyBook.DataAccess/Migrations/20200827024236_InsertIntoDataForCoverType.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BulkyBook.DataAccess.Migrations
+{
+    public partial class InsertIntoDataForCoverType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO CoverTypes(Name) VALUES ('Test')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
