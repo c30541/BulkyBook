96e63e863fe9a45ea4fb4bff555fc8656ec2487e
c30541 <c30541@gmail.com>
2020-08-28 12:07:46 +0800

    section11 - complete

--- BulkyBook.Models/ApplicationUser.cs
+++ BulkyBook.Models/ApplicationUser.cs
@@ -13,15 +13,11 @@ namespace BulkyBook.Models
         [Required]
         public string Name { get; set; }
         public string StreetAddress { get; set; }
-
         public string City { get; set; }
-
         public string State { get; set; }
-
         public string PostalCode { get; set; }
-
         public int? CompanyId { get; set; }
-
+        
         [ForeignKey("CompanyId")]
         public Company Company { get; set; }
 
